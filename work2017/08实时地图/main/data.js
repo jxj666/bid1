daf9256374017c28ccc045706f016481f4ad1ce2
靳小健 <228322991@qq.com>
2017-08-25 11:00:23 +0800

    up

---
+++ work2017/08实时地图/main/data.js
@@ -0,0 +1,590 @@
+var data_map = '['
+  +'{"name":"重庆","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"自贡","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"扬州","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"北京","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"成都","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"亳州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"梧州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"德阳","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"南宁","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"深圳","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"崇左","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"资阳","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"防城港","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"泰安","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"百色","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"开封","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"玉林","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"济宁","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"青岛","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"宿州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"乌鲁木齐","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"泉州","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"赣州","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"大连","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"大理白族自治州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"钦州","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"枣庄","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"四平","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"佛山","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"保定","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"威海","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"哈尔滨","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"延边朝鲜族自治州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"菏泽","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"福州","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"包头","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"合肥","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"呼和浩特","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"上海","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"滁州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"蚌埠","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"鞍山","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"潍坊","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"杭州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"宁德","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"本溪","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"郑州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"广元","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"沧州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"贺州","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"天津","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"通化","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"贵港","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"长春","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"阜阳","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"马鞍山","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"绵阳","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"无锡","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"淄博","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"邯郸","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"宜宾","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"聊城","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"鹤壁","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"达州","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"唐山","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"西安","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"普洱","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"西双版纳傣族自治州","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"牡丹江","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"沈阳","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"景德镇","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"汉中","value":Math.floor(Math.random() * 20)},'
+  +'{"name":"洛阳","value":20 + Math.floor(Math.random() * 10)},'
+  +'{"name":"遵义","value":10 + Math.floor(Math.random() * 10)},'
+  +'{"name":"南京","value":Math.floor(Math.random() * 20)}'
++']';
+
+var map = document.getElementById("map");
+var echart = echarts.init(map);
+
+var topN = 20;
+
+var data = eval(data_map);
+
+var convertData = function (data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        var geoCoord = geoCoordMap[data[i].name];
+        if (geoCoord) {
+            res.push({
+                name: data[i].name,
+                value: geoCoord.concat(data[i].value * 15)
+            });
+        }
+    }
+    return res;
+};
+
+var option = {
+    backgroundColor: 'transparent',
+    title: {
+        text: '二维码营销实时播报大屏',
+        subtext: '成聚移动技术支持',
+        sublink: 'http://www.chengjuiot.com',
+        left: 'center',
+        show: false,
+        textStyle: {
+            color: '#fff'
+        }
+    },
+    tooltip : {
+        formatter: function(key, value) {
+            return key.name + "：" + ((key.data.value[2])/15)
+        },
+        trigger: 'item',
+        show: true
+    },
+    geo: {
+        map: 'china',
+        label: {
+            emphasis: {
+                show: false
+            }
+        },
+        roam: true,
+        //zoom: 2.3,
+        //center: [113,27],
+        itemStyle: {
+            normal: {
+                areaColor: '#7976ac',//'#27273B',// 地图背景色的颜色设置
+                borderColor: '#111'
+            },
+            emphasis: {
+                areaColor: '#413e76'//'#2a333d'
+            }
+        }
+    },
+    series : [
+        {
+            name: '扫码量',
+            type: 'scatter',
+            coordinateSystem: 'geo',
+            data: convertData(data),
+            symbolSize: function (val) {
+                return val[2] / 30;
+            },
+            label: {
+                normal: {
+                    formatter: '{b}',
+                    position: 'right',
+                    show: false
+                },
+                emphasis: {
+                    show: true
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#ddb926'
+                }
+            }
+        },
+        {
+            name: 'Top ' + topN,
+            type: 'effectScatter',
+            coordinateSystem: 'geo',
+            data: convertData(data.sort(function (a, b) {
+                return b.value - a.value;
+            }).slice(0, topN)),
+            symbolSize: function (val) {
+                return val[2] / 30;
+            },
+            showEffectOn: 'render',
+            rippleEffect: {
+                brushType: 'stroke'
+            },
+            hoverAnimation: true,
+            label: {
+                normal: {
+                    formatter: '{b}',
+                    position: 'right',
+                    show: true
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#f4e925',
+                    shadowBlur: 60,
+                    shadowColor: '#333'
+                }
+            },
+            zlevel: 1
+        }
+    ]
+};
+echart.setOption(option, true);
+setInterval(function(){
+  data = eval(data_map);
+  option.series = [
+      {
+        data: convertData(data)
+      },
+      {
+        data: convertData(data.sort(function (a, b) {
+              return b.value - a.value;
+        }).slice(0, topN))
+      }
+  ]
+  echart.setOption(option);
+}, 1500);
+var storage = window.localStorage;
+var sprefix = location.pathname.split('/').join('_');
+
+var _pv_no = 708311;
+var allPeople = 95055;
+var scanner = 100083;
+var packs = 73383;
+var payer = 24610;
+var seller = 687;
+
+// 当前时间
+var count = 0;
+window.setInterval(function(){
+    getNow();
+    getPV();
+    if(count > 5) {
+      getOnline();
+      count = 0;
+    }
+    count++;
+}, 1000)
+
+function getNow() {
+    var date = new Date();
+    var timeReg = /(?:\d*):(?:\d*):(?:\d*)/
+    var _timeStr = timeReg.exec(date);
+    var _html = date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日' + ' ' + _timeStr;
+    document.getElementById("time").innerHTML = _html;
+}
+getNow();
+
+function getPV() {
+  if(storage.getItem(sprefix + '_pv_no')) {
+    _pv_no = JSON.parse(storage.getItem(sprefix + '_pv_no'))
+  }
+  var _pv_html = '';
+  var i2 = 0;
+  var str = _pv_no.toString();
+  for(var i = str.length-1; i >= 0; i--){
+      if(i % 3 == 0 && i != 0){
+          _pv_html += '<span class="sep">' + str[i2] + '</span>';
+      }else{
+          _pv_html += '<span>' + str[i2] + '</span>';
+      }
+      i2++;
+  }
+  $('#pv').html(_pv_html);
+  _pv_no += Math.floor(Math.random() * 30);
+  storage.setItem(sprefix + '_pv_no', JSON.stringify(_pv_no));
+}
+getPV();
+
+function getOnline() {
+  if(storage.getItem(sprefix + '_allPeople')) {
+    allPeople = JSON.parse(storage.getItem(sprefix + '_allPeople'))
+  }
+  if(storage.getItem(sprefix + '_scanner')) {
+    scanner = JSON.parse(storage.getItem(sprefix + '_scanner'))
+  }
+  if(storage.getItem(sprefix + '_packs')) {
+    packs = JSON.parse(storage.getItem(sprefix + '_packs'))
+  }
+  if(storage.getItem(sprefix + '_payer')) {
+    payer = JSON.parse(storage.getItem(sprefix + '_payer'))
+  }
+  if(storage.getItem(sprefix + '_seller')) {
+    seller = JSON.parse(storage.getItem(sprefix + '_seller'))
+  }
+  allPeople += Math.floor(Math.random() * 50);
+  scanner += Math.floor(Math.random() * 50);
+  packs += Math.floor(Math.random() * 50);
+  payer += Math.floor(Math.random() * 50);
+  seller += Math.floor(Math.random() * 50);
+  $("#allPeople").html(allPeople);
+  if(location.pathname.indexOf("615") !== -1) {
+    $("#onlinePeople").css({display:"inline"}).find('em').html(2000 + Math.floor(Math.random() * 1000));
+  }
+  $("#scanner").html(scanner);
+  $("#packs").html(packs);
+  $("#payer").html(payer);
+  $("#seller").html(seller);
+  storage.setItem(sprefix + '_allPeople', JSON.stringify(allPeople));
+  storage.setItem(sprefix + '_scanner', JSON.stringify(scanner));
+  storage.setItem(sprefix + '_packs', JSON.stringify(packs));
+  storage.setItem(sprefix + '_payer', JSON.stringify(payer));
+  storage.setItem(sprefix + '_seller', JSON.stringify(seller));
+  /*
+  $("#desc").html('已有<em class="color">' 
+    + allPeople
+    + '</em>人参与，当前在线<em>' 
+    + Math.floor(Math.random() * 500)
+    + '</em>人，累计验真<em class="color">'
+    + scanner
+    + '</em>条，<em class="color">' 
+    + packs
+    + '</em>小包，累计消费者<em class="color">'
+    + payer
+    + '</em>人，零售户<em class="color">' 
+    + seller
+    + '</em>人。');
+    */
+}
+getOnline();
+var data_pv = [
+  {
+      "city": "重庆",
+      "pv": 11842,
+  }, {
+      "city": "成都",
+      "pv": 9057,
+  }, {
+      "city": "深圳",
+      "pv": 8797,
+  }, {
+      "city": "青岛",
+      "pv": 7972,
+  }, {
+      "city": "广州",
+      "pv": 7257,
+  }, {
+      "city": "北京",
+      "pv": 6188,
+  }, {
+      "city": "济宁",
+      "pv": 5907,
+  }, {
+      "city": "南宁",
+      "pv": 5865,
+  }, {
+      "city": "未知",
+      "pv": 5826,
+  }, {
+      "city": "上海",
+      "pv": 5672,
+  }, {
+      "city": "哈尔滨",
+      "pv": 5173,
+  }, {
+      "city": "郑州",
+      "pv": 5037,
+  }, {
+      "city": "天津",
+      "pv": 4806,
+  }, {
+      "city": "潍坊",
+      "pv": 4104,
+  }, {
+      "city": "洛阳",
+      "pv": 4046,
+  }, {
+      "city": "石家庄",
+      "pv": 4036,
+  }, {
+      "city": "东莞",
+      "pv": 3761,
+  }, {
+      "city": "沧州",
+      "pv": 3677,
+  }, {
+      "city": "玉林",
+      "pv": 3538,
+  }, {
+      "city": "南京",
+      "pv": 3289,
+  }, {
+      "city": "太原",
+      "pv": 3190,
+  }, {
+      "city": "武汉",
+      "pv": 3169,
+  }, {
+      "city": "苏州",
+      "pv": 3117,
+  }, {
+      "city": "大连",
+      "pv": 3072,
+  }, {
+      "city": "西安",
+      "pv": 2976,
+  }, {
+      "city": "唐山",
+      "pv": 2839,
+  }, {
+      "city": "济南",
+      "pv": 2781,
+  }, {
+      "city": "长春",
+      "pv": 2770,
+  }, {
+      "city": "保定",
+      "pv": 2767,
+  }, {
+      "city": "沈阳",
+      "pv": 2674,
+  }];
+var data_uv = [
+  {
+      "city": "青岛",
+      "uv": 1276,
+  }, {
+      "city": "重庆",
+      "uv": 1243,
+  }, {
+      "city": "成都",
+      "uv": 1049,
+  }, {
+      "city": "深圳",
+      "uv": 832,
+  }, {
+      "city": "南宁",
+      "uv": 789,
+  }, {
+      "city": "广州",
+      "uv": 642,
+  }, {
+      "city": "济宁",
+      "uv": 642,
+  }, {
+      "city": "上海",
+      "uv": 609,
+  }, {
+      "city": "北京",
+      "uv": 604,
+  }, {
+      "city": "潍坊",
+      "uv": 603,
+  }, {
+      "city": "未知",
+      "uv": 578,
+  }, {
+      "city": "天津",
+      "uv": 508,
+  }, {
+      "city": "玉林",
+      "uv": 508,
+  }, {
+      "city": "郑州",
+      "uv": 462,
+  }, {
+      "city": "哈尔滨",
+      "uv": 461,
+  }, {
+      "city": "东莞",
+      "uv": 423,
+  }, {
+      "city": "洛阳",
+      "uv": 359,
+  }, {
+      "city": "贺州",
+      "uv": 355,
+  }, {
+      "city": "长春",
+      "uv": 353,
+  }, {
+      "city": "大连",
+      "uv": 353,
+  }, {
+      "city": "沧州",
+      "uv": 349,
+  }, {
+      "city": "唐山",
+      "uv": 345,
+  }, {
+      "city": "南京",
+      "uv": 340,
+  }, {
+      "city": "昆明",
+      "uv": 322,
+  }, {
+      "city": "西安",
+      "uv": 319,
+  }, {
+      "city": "临沂",
+      "uv": 307,
+  }, {
+      "city": "苏州",
+      "uv": 305,
+  }, {
+      "city": "钦州",
+      "uv": 294,
+  }, {
+      "city": "太原",
+      "uv": 294,
+  }, {
+      "city": "济南",
+      "uv": 294,
+  }];
+
+var _html_pv = '';
+var _html_uv = '';
+var _first = $("#qrcode_time");
+var _second = $("#winner_number");
+
+function getTopDataN() {
+  if(storage.getItem(sprefix + '_data_pv')) {
+    data_pv = JSON.parse(storage.getItem(sprefix + '_data_pv'))
+  }
+  data_pv.forEach(function(n, index){
+    n.pv += Math.floor(Math.random() * 55);
+  })
+  data_pv.sort(function(a, b){
+    return b.pv - a.pv;
+  }).forEach(function(n, index){
+      if ( index === 0 ) {
+        _html_pv += '<li class="city-wrap">';
+      } else if (index%10 === 0) {
+        _html_pv += '</li><li class="city-wrap">';
+      }
+      if(location.pathname.indexOf("615") === -1) {
+        _html_pv += '<div class="city"><span><i>' + (index + 1) + ' </i>' + n.city + '</span> <em>' + n.pv.toString() + '</em></div>';
+      } else {
+        _html_pv += '<div class="city"><span><i>' + (index + 1) + ' </i>' + n.city + '</span> <em>' + n.pv + '</em></div>';
+      }
+      if ( index === (data_pv.length - 1) ) {
+        _html_pv += '</li>'
+      }
+  })
+  _first.html(_html_pv);
+  storage.setItem(sprefix + '_data_pv', JSON.stringify(data_pv));
+
+  if(storage.getItem(sprefix + '_data_uv')) {
+    data_pv = JSON.parse(storage.getItem(sprefix + '_data_uv'))
+  }
+  data_uv.forEach(function(n, index){
+    n.uv += Math.floor(Math.random() * 3);
+  })
+  data_uv.sort(function(a, b){
+    return b.uv - a.uv;
+  }).forEach(function(n, index){
+      if ( index === 0 ) {
+        _html_uv += '<li class="city-wrap">';
+      } else if (index%10 === 0) {
+        _html_uv += '</li><li class="city-wrap">';
+      }
+      if(location.pathname.indexOf("615") === -1) {
+        _html_uv += '<div class="city"><span><i>' + (index + 1) + ' </i>' + n.city + '</span> <em>' + n.uv.toString()  + '</em></div>';
+      } else {
+        _html_uv += '<div class="city"><span><i>' + (index + 1) + ' </i>' + n.city + '</span> <em>' + n.uv + '</em></div>';
+      }
+      if ( index === (data_uv.length - 1) ) {
+        _html_uv += '</li>'
+      }
+  })
+  _second.html(_html_uv);
+  storage.setItem(sprefix + '_data_uv', JSON.stringify(data_pv));
+}
+getTopDataN();
+
+var _time = 6000;//定义滚动间隙时间
+var _scrolling;//需要清除的动画
+var _times = 0;
+_scrolling = setInterval(function () {
+  if(_times > 3) {
+    //getTopDataN();
+    _times = 0;
+  }
+  _times++;
+    // var _field = _ele.find('.city-wrap:first');//此变量不可放置于函数起始处,li:first取值是变化的
+    var _firstField = _first.find('.city-wrap:first');
+    var _secondField = _second.find('.city-wrap:first');
+    // var _h = _field.height();//取得每次滚动高度(多行滚动情况下,此变量不可置于开始处,否则会有间隔时长延时)
+    var _firstH = _firstField.height();
+    var _secondH = _secondField.height();
+    _firstField.animate({
+        marginTop: -_firstH
+    }, 600, function () {
+        _firstField.css('marginTop', 0).appendTo(_first);
+    });
+    _secondField.animate({
+        marginTop: -_secondH
+    }, 600,function () {
+        _secondField.css('marginTop', 0).appendTo(_second);
+    });
+    // _field.animate({
+    //     marginTop: -_h
+    // }, 600, function () {//通过取负margin值,隐藏第一行
+    //     _field.css('marginTop', 0).appendTo(_ele);//隐藏后,将该行的margin值置零,并插入到最后,实现无缝滚动
+    // })
+}, _time);//滚动间隔时间取决于_interval
