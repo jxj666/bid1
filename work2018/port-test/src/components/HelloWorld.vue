e84c1adb3569dfc03869cf98a0e8daf6cd695f99
异阳 <228322991@qq.com>
2018-01-03 14:28:26 +0800

    up

--- work2018/port-test/src/components/HelloWorld.vue
+++ work2018/port-test/src/components/HelloWorld.vue
@@ -3,32 +3,51 @@
     <h1>{{ msg }}</h1>
   </div>
 </template>
-
 <script>
+import axios from 'axios'
+import { Base64 } from 'js-base64'
+
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: '接口测试'
+      msg: '接口测试1'
     }
+  },
+  created() {
+    axios({
+        method: 'post',
+        url: 'https://testweiop.chengjuiot.com/act/rule/JS0001DS001?name=authentication ',
+        data: {
+          keyStr: Base64.encode('123456789')
+        }
+      })
+      .then(function(response) {
+        console.log(response)
+      })
   }
 }
-</script>
 
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
+
 </style>
